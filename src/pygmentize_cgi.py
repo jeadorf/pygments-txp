78e5355f3a56d7f3460092cce64750d7a73378e3
Julius Adorf <jeadorf@gmail.com>
2010-04-12 21:11:37 +0200

    url whitelist regex for CGI

--- src/pygmentize_cgi.py
+++ src/pygmentize_cgi.py
@@ -1,38 +1,88 @@
 #!/usr/bin/env python
+""" A python CGI serving as a syntax highlighting engine. Based on Pygments.
+Input parameters:
+
+- url       The resource which provides the code to highlight
+- lang      The language of the code (python, css, etc.). See Pygments
+            documentation for all supported languages.
+
+Output is formatted in HTML. Styles are included. This is likely to change with
+future versions.
+
+You can customize the CGI's behaviour by editing a configuration file. Its name
+is 'pygmentize_cgi.cfg' and it should be located under the same directory where
+this CGI resides. Valid sections and properties are listed in the following using
+a pseudo-notation. For default values, see the code.
+
+[security]
+# If true the script is executed only if REMOTE_ADDR and
+# SERVER_ADDR match.
+allowRemoteAcess = True | False
+# If specified only URLs that match a whitelist regular expression
+# are allowed as input for the highlighter. Empty string allows
+# everything. Allows any URLs by default. You might change it to
+# something like file:///var/www/files/([a-zA-Z0-9_i\\-]+(\\.[a-zA-Z0-9_\\-]+)*\\.?/?)*
+whitelistRegex = <REGEX>
+"""
 
 import cgi
 import cgitb
-
 cgitb.enable()
 
 from pygments import highlight
-from pygments.lexers import get_lexer_by_name 
+from pygments.lexers import get_lexer_by_name
 from pygments.formatters import HtmlFormatter
 
 import urllib2
+import os
+import re
+import ConfigParser
+
+class _SecurityException(Exception):
+    def __init__(self, msg):
+        self.msg = msg
+
+def _read_cfg():
+    cfg = ConfigParser.SafeConfigParser({
+        'allowRemoteAccess' : 'False',
+        'whitelistRegex' : ''
+    })
+    cfg.add_section('security')
+    cfg.read('pygmentize_cgi.cfg')
+    return cfg
+
+def _security_check(cfg, url):
+    allowRemoteAccess = cfg.getboolean('security', 'allowRemoteAccess')
+    whitelistRegex = cfg.get('security', 'whitelistRegex')
+    if not allowRemoteAccess:
+        if os.environ['REMOTE_ADDR'] != os.environ['SERVER_ADDR']:
+            raise SecurityException('Error: Remote address must match server address.')
+    if not whitelistRegex == '':
+        m = re.match(whitelistRegex, url)
+        if not m or m.group(0) != url:
+            raise _SecurityException('Error: Illegal URL, not matched by whitelist RE.')
 
 def main():
-    print "Content-type: text/html"
-    print
-    print "<html><head>"
-
+    cfg = _read_cfg()
     form = cgi.FieldStorage()
-
     lang = form.getvalue('lang')
     url = form.getvalue('url')
 
+    code = urllib2.urlopen(url).read()
     lexer = get_lexer_by_name(lang)
     formatter = HtmlFormatter()
-    code = urllib2.urlopen(url).read() 
-
-    print "<style><!--"
-    print formatter.get_style_defs('.highlight')
-    print "--></style>"
-    print "</head><body>"
-    print highlight(code, lexer, formatter)
-    print "</body></html>"
+    print 'Content-type: text/html'
     print
 
-if __name__ == "__main__":
+    _security_check(cfg, url)
+
+    print '<style><!--'
+    print formatter.get_style_defs('.highlight')
+    print '--></style>'
+    print
+    print highlight(code, lexer, formatter)
+    print
+
+if __name__ == '__main__':
     main()
 
